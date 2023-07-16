6db29f20e61235ad5d2c4db484723247955e251d
Esperanza <Esperanza@LAPTOP-8N9I2DD0>
2023-07-16 18:43:29 -0500

    Agregar archivos de proyecto.

---
+++ esuquilloSem7/DataBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace esuquilloSem7
+{
+    public interface DataBase
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
---
+++ esuquilloSem7/clienteAndroid.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using System.IO;
+using esuquilloSem7;
+
+[assembly:Xamarin.Forms.Dependency(typeof(clienteAndroid))]
+
+namespace esuquilloSem7
+{
+    public class clienteAndroid : DataBase
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var ruta = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var rutaBase = Path.Combine(ruta,"uisrael.db3");
+            return new SQLiteAsyncConnection(rutaBase);
+        }
+    }
+}
